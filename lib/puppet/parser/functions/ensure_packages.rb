840e8621ce36a97c0af002b3d752dcfee4192956
Matt Palmer <mpalmer@hezmatt.org>
2013-11-04 10:03:42 +1100

    Reduce insanity in ensure_packages function

--- lib/puppet/parser/functions/ensure_packages.rb
+++ lib/puppet/parser/functions/ensure_packages.rb
@@ -1,5 +1,17 @@
+# Copyright 2011 Puppet Labs Inc
+# Copyright 2013 Matt Palmer <matt@hezmatt.org>
 #
-# ensure_packages.rb
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.  You may obtain a
+# copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations
+# under the License.
 #
 require 'puppet/parser/functions'
 
@@ -8,17 +20,16 @@ module Puppet::Parser::Functions
 Takes a list of packages and only installs them if they don't already exist.
     EOS
   ) do |arguments|
-
-    raise(Puppet::ParseError, "ensure_packages(): Wrong number of arguments " +
-      "given (#{arguments.size} for 1)") if arguments.size != 1
-    raise(Puppet::ParseError, "ensure_packages(): Requires array " +
-      "given (#{arguments[0].class})") if !arguments[0].kind_of?(Array)
+    pkglist = arguments.flatten
 
     Puppet::Parser::Functions.function(:ensure_resource)
-    arguments[0].each { |package_name|
+    pkglist.each do |package_name|
+      unless package_name.is_a? String
+        raise ArgumentError,
+              "ensure_packages(): Package name must be a string (you gave me #{package_name.inspect})"
+      end
+
       function_ensure_resource(['package', package_name, {'ensure' => 'present' } ])
-    }
+    end
   end
 end
-
-# vim: set ts=2 sw=2 et :
