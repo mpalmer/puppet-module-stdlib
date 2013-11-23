be22b8d448c6c1346e69597f663ab00427c08a14
Matt Palmer <mpalmer@hezmatt.org>
2013-11-24 10:53:49 +1100

    New function: reject

--- lib/puppet/parser/functions/reject.rb
+++ lib/puppet/parser/functions/reject.rb
@@ -1,31 +1,30 @@
+# Copyright 2010-2013 Anchor Systems Pty Ltd
 #
-# reject.rb
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.  You may obtain a
+# copy of the License at
 #
-
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
 module Puppet::Parser::Functions
-  newfunction(:reject, :type => :rvalue, :doc => <<-EOS) do |args|
-This function searches through an array and rejects all elements that match
-the provided regular expression.
+	newfunction(:reject, :type => :rvalue, :doc => "Given an array and a regexp, returns the elements of the array that do not match.") do |args|
+		args.length == 2 or raise Puppet::ParseError.new("reject: expecting 2 arguments, got #{args.length}")
+		(a, r) = args
 
-*Examples:*
+		a.is_a? Array or raise Puppet::ParseError.new("reject: invalid first argument: #{a} (expecting Array, got #{a.class})")
+		is_scalar = Puppet::Parser::Functions.function(:is_scalar) or raise Puppet::Error.new("reject: is_scalar could not be loaded")
+		send(is_scalar, [r]) or raise Puppet::ParseError.new("reject: invalid second argument: #{r} (expecting scalar, got #{r.class})")
 
-    reject(['aaa','bbb','ccc','aaaddd'], 'aaa')
-
-Would return:
-
-    ['bbb','ccc']
-EOS
-
-    if (args.size != 2)
-      raise Puppet::ParseError,
-        "reject(): Wrong number of arguments given #{args.size} for 2"
-    end
-
-    ary = args[0]
-    pattern = Regexp.new(args[1])
-
-    ary.reject { |e| e =~ pattern }
-  end
+		regexp = Regexp.compile(r.to_s)
+		a.reject do |e|
+			send(is_scalar, [e]) or raise Puppet::ParseError.new("reject: invalid array element: #{e} (expecting scalar, got #{e.class})")
+			e.to_s =~ regexp
+		end
+	end
 end
-
-# vim: set ts=2 sw=2 et :
